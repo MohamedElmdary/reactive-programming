3572780d5fbf319ab3143fcf2a8d1b231df74f81
MohamedElmdary <engm5081@gmail.com>
2019-11-23 10:43:50 +0200

    allow infer type from pipe

--- src/observable.ts
+++ src/observable.ts
@@ -32,16 +32,16 @@ class Observable<T> {
     }
 
     public pipe(): Observable<T>;
-    public pipe<A>(fn1: Operator<T, A>): Observable<A>;
-    public pipe<A, B>(fn1: Operator<T, A>, fn2: Operator<A, B>): Observable<B>;
-    public pipe<A, B, C>(fn1: Operator<T, A>, fn2: Operator<A, B>, fn3: Operator<B, C>): Observable<C>; // prettier-ignore
-    public pipe<A, B, C, D>(fn1: Operator<T, A>, fn2: Operator<A, B>, fn3: Operator<B, C>, fn4: Operator<C, D>): Observable<D>; // prettier-ignore
-    public pipe<A, B, C, D, E>(fn1: Operator<T, A>, fn2: Operator<A, B>, fn3: Operator<B, C>, fn4: Operator<C, D>, fn5: Operator<D, E>): Observable<E>; // prettier-ignore
-    public pipe<A, B, C, D, E, F>(fn1: Operator<T, A>, fn2: Operator<A, B>, fn3: Operator<B, C>, fn4: Operator<C, D>, fn5: Operator<D, E>, fn6: Operator<E, F>): Observable<F>; // prettier-ignore
-    public pipe<A, B, C, D, E, F, G>(fn1: Operator<T, A>, fn2: Operator<A, B>, fn3: Operator<B, C>, fn4: Operator<C, D>, fn5: Operator<D, E>, fn6: Operator<E, F>, fn7: Operator<F, G>): Observable<G>; // prettier-ignore
-    public pipe<A, B, C, D, E, F, G, H>(fn1: Operator<T, A>, fn2: Operator<A, B>, fn3: Operator<B, C>, fn4: Operator<C, D>, fn5: Operator<D, E>, fn6: Operator<E, F>, fn7: Operator<F, G>, fn8: Operator<G, H>): Observable<H>; // prettier-ignore
-    public pipe<A, B, C, D, E, F, G, H, I>(fn1: Operator<T, A>, fn2: Operator<A, B>, fn3: Operator<B, C>, fn4: Operator<C, D>, fn5: Operator<D, E>, fn6: Operator<E, F>, fn7: Operator<F, G>, fn8: Operator<G, H>, fn9: Operator<H, I>): Observable<I>; // prettier-ignore
-    public pipe<A, B, C, D, E, F, G, H, I>(fn1: Operator<T, A>, fn2: Operator<A, B>, fn3: Operator<B, C>, fn4: Operator<C, D>, fn5: Operator<D, E>, fn6: Operator<E, F>, fn7: Operator<F, G>, fn8: Operator<G, H>, fn9: Operator<H, I>, fn10: Operator<I, any>, ...fns: Array<Operator<any, any>>): Observable<any>; // prettier-ignore
+    public pipe<A>(operator1: Operator<T, A>): Observable<A>;
+    public pipe<A, B>(operator1: Operator<T, A>, operator2: Operator<A, B>): Observable<B>; // prettier-ignore
+    public pipe<A, B, C>(operator1: Operator<T, A>, operator2: Operator<A, B>, operator3: Operator<B, C>): Observable<C>; // prettier-ignore
+    public pipe<A, B, C, D>(operator1: Operator<T, A>, operator2: Operator<A, B>, operator3: Operator<B, C>, operator4: Operator<C, D>): Observable<D>; // prettier-ignore
+    public pipe<A, B, C, D, E>(operator1: Operator<T, A>, operator2: Operator<A, B>, operator3: Operator<B, C>, operator4: Operator<C, D>, operator5: Operator<D, E>): Observable<E>; // prettier-ignore
+    public pipe<A, B, C, D, E, F>(operator1: Operator<T, A>, operator2: Operator<A, B>, operator3: Operator<B, C>, operator4: Operator<C, D>, operator5: Operator<D, E>, operator6: Operator<E, F>): Observable<F>; // prettier-ignore
+    public pipe<A, B, C, D, E, F, G>(operator1: Operator<T, A>, operator2: Operator<A, B>, operator3: Operator<B, C>, operator4: Operator<C, D>, operator5: Operator<D, E>, operator6: Operator<E, F>, operator7: Operator<F, G>): Observable<G>; // prettier-ignore
+    public pipe<A, B, C, D, E, F, G, H>(operator1: Operator<T, A>, operator2: Operator<A, B>, operator3: Operator<B, C>, operator4: Operator<C, D>, operator5: Operator<D, E>, operator6: Operator<E, F>, operator7: Operator<F, G>, operator8: Operator<G, H>): Observable<H>; // prettier-ignore
+    public pipe<A, B, C, D, E, F, G, H, I>(operator1: Operator<T, A>, operator2: Operator<A, B>, operator3: Operator<B, C>, operator4: Operator<C, D>, operator5: Operator<D, E>, operator6: Operator<E, F>, operator7: Operator<F, G>, operator8: Operator<G, H>, operator9: Operator<H, I>): Observable<I>; // prettier-ignore
+    public pipe<A, B, C, D, E, F, G, H, I>(operator1: Operator<T, A>, operator2: Operator<A, B>, operator3: Operator<B, C>, operator4: Operator<C, D>, operator5: Operator<D, E>, operator6: Operator<E, F>, operator7: Operator<F, G>, operator8: Operator<G, H>, operator9: Operator<H, I>, operator10: Operator<I, any>, ...operators: Array<Operator<any, any>>): Observable<any>; // prettier-ignore
     public pipe(...operators: Array<Operator<any, any>>): Observable<any> {
         let currentObservable: Observable<any> = this;
         operators.forEach(operator => {
